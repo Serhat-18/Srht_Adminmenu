6843d84b993c7f6af9a901f911e61918de94dcf5
$erhat/18 <79026362+Serhat-18@users.noreply.github.com>
2021-06-24 01:59:02 +0300

    Add files via upload

---
+++ srht_admin/config.lua
@@ -0,0 +1,7 @@
+Config          = {}
+
+Config.webhook = 'WEBHOOK GİRİNİZ' -- Log webhooku
+Config.kickmesaj = 'Sunucuya Restart Atılıcaktır MADE BY $erhat/18' -- Menuden herkesi attığınızdaki kick mesajı
+Config.menutus = 178 -- Menu hangi tuşla açılsın
+Config.yukaritus = 32 -- Noclip yukarı çıkma (W)
+Config.assagitus = 269 -- Noclip aşşağı inme (S)
